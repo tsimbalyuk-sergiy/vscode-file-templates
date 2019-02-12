c590ec3dfe471393bf5e66c072fa26137b4a73c9
Sergiy Tsimbalyuk <sv.tsimbalyuk@gmail.com>
2019-02-12 14:56:11 +0200

    added java and groovy templates handling

--- commands/newfilefromtemplate.js
+++ commands/newfilefromtemplate.js
@@ -1,123 +1,153 @@
+/* eslint-disable require-jsdoc */
 const TM = require('../templatemanager');
 const vscode = require('vscode');
 const fs = require('fs');
-const editTemplate = require('./edittemplate');
-const newTemplate = require('./newtemplate');
 const path = require('path');
 
-const DATE_TOKEN  = '${DATE}';
+const DATE_TOKEN = '${DATE}';
 const AUTHOR_TOKEN = '${AUTHOR}';
+const FILE_TOKEN = /\$\{FILE\}/g;
+const FILENAME_TOKEN = /\$\{FILENAME\}/g;
+const PACKAGE_TOKEN = /\$\{PACKAGE\}/g;
+
 const NEW_FILE = 'Files: New File';
-const CREATE_TEMPLATE = 'Files: New File Template';
-const EDIT_TEMPLATE = 'Files: Edit File Template';
-const TEMPLATES_PREFIX = 'Template: ';
+const FILE_EXISTS_MESSAGE = 'File already exists with the same name.';
 
+function createFile(filepath, data = '', extension = '') {
+  const input = vscode.window.showInputBox({
+    prompt: 'Enter new fileName',
+  });
 
-function createFile(filepath, data = '', extension = ''){
-    let input = vscode.window.showInputBox({
-        prompt:'Enter new fileName'
-    });
-    input.then( filename => {
-        if(!filename){
-            vscode.window.showErrorMessage('Please retry with a valid filename');
-            return;
+  input.then((filename) => {
+    if (!filename) {
+      vscode.window.showErrorMessage('Please retry with a valid filename');
+      return;
+    }
+    const filenameWithExt = filename + extension;
+    fs.stat(filepath, (err, stats) => {
+      let curDir = filepath;
+      if (!stats.isDirectory()) {
+        curDir = path.dirname(filepath);
+      }
+
+      const newFilePath = path.join(curDir, filenameWithExt);
+      const config = vscode.workspace.getConfiguration('templates');
+      data = data.replace(AUTHOR_TOKEN, config.Author);
+      data = data.replace(DATE_TOKEN, new Date().toDateString());
+      data = data.replace(FILENAME_TOKEN, filename);
+      data = data.replace(FILE_TOKEN, filenameWithExt);
+      const isPartOfProject = filepath.includes('src/main');
+      if (filenameWithExt.endsWith('java') && isPartOfProject) {
+        const tempPkg = filepath.replace(new RegExp('.*' + 'main/java/'), '');
+        const packageName = tempPkg.replace(/\//g, '.');
+        data = data.replace(PACKAGE_TOKEN, packageName+';');
+      } else if (filenameWithExt.endsWith('java') && !isPartOfProject) {
+        const tempPkg = filepath
+            .replace(/^\//g, '')
+            .replace(/[^\/]*$/, '')
+            .replace(/\/$/, '');
+        const packageName = tempPkg.replace(/\//g, '.');
+        data = data.replace(PACKAGE_TOKEN, packageName+';');
+      }
+      if (filenameWithExt.endsWith('groovy') && isPartOfProject) {
+        const tempPkg = filepath.replace(new RegExp('.*' + 'main/groovy/'), '');
+        const packageName = tempPkg.replace(/\//g, '.');
+        data = data.replace(PACKAGE_TOKEN, packageName);
+      } else if (filenameWithExt.endsWith('groovy') && !isPartOfProject) {
+        const tempPkg = filepath
+            .replace(/^\//g, '')
+            .replace(/[^\/]*$/, '')
+            .replace(/\/$/, '');
+        const packageName = tempPkg.replace(/\//g, '.');
+        data = data.replace(PACKAGE_TOKEN, packageName);
+      }
+
+      fs.stat(newFilePath, (err, stats) => {
+        if (stats && stats.isFile()) {
+          vscode.window.showErrorMessage(FILE_EXISTS_MESSAGE);
+          return;
         }
-
-        let filenameWithExt = filename + extension;
-        fs.stat(filepath, (err, stats) => {
-            let curDir = filepath;
-            if(!stats.isDirectory())
-                curDir = path.dirname(filepath);
-
-            let newFilePath = path.join(curDir, filenameWithExt);
-            let config = vscode.workspace.getConfiguration('templates');
-            data = data.replace(AUTHOR_TOKEN, config.Author);
-            data = data.replace(DATE_TOKEN, new Date().toDateString());
-
-            fs.stat(newFilePath, (err, stats) => {
-                
-                if(stats && stats.isFile()){
-                    vscode.window.showErrorMessage('File already exists with the same name. Please retry with another name');
-                    return;
-                }
-
-                fs.writeFile(newFilePath, data, (err) => {
-                    if(err){
-                        vscode.window.showErrorMessage('Cannot create new file');
-                        return;
-                    }
-
-                    vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('file://' + newFilePath)).then(() => {
-                        console.log('Document opened');
-                    });
-                });
-            });
-        });
-    });
-}
-
-function constructTemplatesOptions(templatesInfo){
-
-    let templateMenuOptions = [{label:NEW_FILE}];
-    Object.keys(templatesInfo).forEach( template => {
-        let templateName = path.basename(templatesInfo[template]);
-        let extStart = templateName.lastIndexOf('.');
-        templateMenuOptions.push({label:template.charAt(0).toUpperCase() + template.slice(1), extension: templateName.substring(extStart)});
-    });
-
-    // templateMenuOptions = templateMenuOptions.concat([{label:CREATE_TEMPLATE} ,{label:EDIT_TEMPLATE}]);
-
-    return templateMenuOptions;
-}
-
-function createNewFile(info){
-    let currentPath = info._path;
-    if(!currentPath){
-        let editor = vscode.window.activeTextEditor;
-        if(editor)
-            currentPath = editor.document.fileName;
-        
-        if(!currentPath)
-            currentPath = vscode.workspace.rootPath;
-
-        // TODO : create untitled file from template
-        if(!currentPath)
+        fs.writeFile(newFilePath, data, (err) => {
+          if (err) {
+            vscode.window.showErrorMessage('Cannot create new file');
             return;
+          }
+          vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('file://' + newFilePath)).then(() => {
+            console.log('Document opened');
+          });
+        });
+      });
+    });
+  });
+}
+
+function constructTemplatesOptions(templatesInfo) {
+  const templateMenuOptions = [{
+    label: NEW_FILE,
+  }];
+  Object.keys(templatesInfo).forEach((template) => {
+    const templateName = path.basename(templatesInfo[template]);
+    const extStart = templateName.lastIndexOf('.');
+    templateMenuOptions.push({
+      label: template.charAt(0).toUpperCase() + template.slice(1),
+      extension: templateName.substring(extStart),
+    });
+  });
+  return templateMenuOptions;
+}
+
+function createNewFile(info) {
+  let currentPath = info.path;
+  if (!currentPath) {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      currentPath = editor.document.fileName;
     }
 
-    TM.getTemplates().then( templatesInfo => {
-        let templateMenuOptions = constructTemplatesOptions(templatesInfo);
-        let select = vscode.window.showQuickPick(templateMenuOptions, {
-            placeHolder:'Select a template to create from'
-        });
+    if (!currentPath) {
+      currentPath = vscode.workspace.rootPath;
+    }
 
-        select.then(option => {
-            if(!option)
-                return;
+    // TODO : create untitled file from template
+    if (!currentPath) {
+      return;
+    }
+  }
 
-            if(!option.extension){
-                // if(option.label === CREATE_TEMPLATE)
-                //     newTemplate.newTemplate('');
-                // else if(option.label === EDIT_TEMPLATE){
-                //     editTemplate();
-                // }
-                if(option.label === NEW_FILE)
-                    createFile(currentPath);
-
-                return;
-            }
-
-            fs.readFile(templatesInfo[option.label], 'utf8', (err, data) => {
-                if(err){
-                    vscode.window.showErrorMessage('Cannot find the template');
-                    return;
-                }
-
-                createFile(currentPath, data, option.extension);
-            });
-
-        });
+  TM.getTemplates().then((templatesInfo) => {
+    const templateMenuOptions = constructTemplatesOptions(templatesInfo);
+    const select = vscode.window.showQuickPick(templateMenuOptions, {
+      placeHolder: 'Select a template to create from',
     });
+
+    select.then((option) => {
+      if (!option) {
+        return;
+      }
+
+      if (!option.extension) {
+        // if(option.label === CREATE_TEMPLATE)
+        //     newTemplate.newTemplate('');
+        // else if(option.label === EDIT_TEMPLATE){
+        //     editTemplate();
+        // }
+        if (option.label === NEW_FILE) {
+          createFile(currentPath);
+        }
+
+        return;
+      }
+
+      fs.readFile(templatesInfo[option.label], 'utf8', (err, data) => {
+        if (err) {
+          vscode.window.showErrorMessage('Cannot find the template');
+          return;
+        }
+
+        createFile(currentPath, data, option.extension);
+      });
+    });
+  });
 }
 
 module.exports = createNewFile;
---
+++ templates/Java.java
@@ -0,0 +1,9 @@
+package ${PACKAGE}
+/******************************************
+*  Author : ${AUTHOR}   
+*  Created On : ${DATE}
+*  File : ${FILE}
+*******************************************/
+class ${FILENAME} {
+    public ${FILENAME}(){}
+}
